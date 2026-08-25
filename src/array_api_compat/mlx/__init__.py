90a7d45e283413a0b8b90e76ae25c4c8724489e7
Declan Healy <dfh584@student.bham.ac.uk>
2026-08-25 13:02:47 +0100

    Expose MLX dtype constants statically

--- src/array_api_compat/mlx/__init__.py
+++ src/array_api_compat/mlx/__init__.py
@@ -1,13 +1,28 @@
 from typing import Final
 
+import mlx.core as _mx
+
 from .._internal import clone_module
+
+bool_: Final = _mx.bool_
+int8: Final = _mx.int8
+int16: Final = _mx.int16
+int32: Final = _mx.int32
+int64: Final = _mx.int64
+uint8: Final = _mx.uint8
+uint16: Final = _mx.uint16
+uint32: Final = _mx.uint32
+uint64: Final = _mx.uint64
+float32: Final = _mx.float32
+float64: Final = _mx.float64
+complex64: Final = _mx.complex64
 
 __all__ = clone_module("mlx.core", globals())
 
 from . import _aliases, _overrides
 from ._aliases import *  # type: ignore[assignment,no-redef] # noqa: F403
 from ._overrides import *  # type: ignore[assignment,no-redef] # noqa: F403
-from ._info import __array_namespace_info__
+from ._info import __array_namespace_info__ as __array_namespace_info__
 
 # Import the compatibility submodules explicitly so they replace the native
 # ``mlx.core.fft`` and ``mlx.core.linalg`` objects cloned above.
